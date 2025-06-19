9d44c990c26c53191bcededaa1b11c7a7a194c29
abhirup2565 <abhirup2565@gmail.com>
2025-06-19 10:27:19 +0530

    animated hamburger

--- app/js/script.js
+++ app/js/script.js
@@ -1,1 +1,22 @@
-console.log("hello world")
+const ham = document.querySelector('#btn-ham');
+const header = document.querySelector('.header');
+const overlay = document.querySelector('#overlay');
+
+ham.addEventListener('click', function()
+{
+    console.log('ham clicked');
+    if(header.classList.contains('ham_open'))
+    {
+        overlay.classList.remove('fadein');
+        header.classList.remove('ham_open');
+        overlay.classList.remove('overlay');
+        overlay.classList.add('fadeout');
+    }
+    else
+    {
+        overlay.classList.remove('fadeout');
+        overlay.classList.add('fadein');
+        header.classList.add('ham_open');
+        overlay.classList.add('overlay');
+    }
+});
